bffa48fb0da1d0d548cb63dca669ed92aff10666
maxig13 <lemaxg98@gmail.com>
2022-09-28 23:48:31 -0300

    pre-entrega1

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = (props) => {
+    console.log('navbar')
+    return (
+        <div>NavBar
+        <li><button>Inicio</button></li>
+        <li><button>Eventos</button></li>
+        <li><button>Galeria</button></li>
+        <li><button>Tienda</button></li>
+        </div>
+        
+    )
+}
+
+export default NavBar
